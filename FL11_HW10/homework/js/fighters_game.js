47a64941d29ffc35b798ebcf9fd742decce41953
7art <sem85cv@yandex.ru>
2019-07-21 23:38:35 +0300

    HW10 optimization code

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -21,9 +21,9 @@ let Fighter = function (object) {
             return totalHp;
         },
         attack: function (defender) {
-            let band = 100;
-            let attackSuccess = Math.random() * band - defender.getAgility();
-            if (attackSuccess > defender.getAgility()) {
+            let band = 101;
+            let attackSuccess = Math.round(Math.random() * band);
+            if (attackSuccess >= defender.getAgility()) {
                 defender.dealDamage(damage);
                 console.log(`${name} make ${damage} damage to ${defender.getName()}`);
             } else {
@@ -34,16 +34,10 @@ let Fighter = function (object) {
             return `Name ${name}, Wins ${wins}, Losses ${losses}`;
         },
         heal: function (extra) {
-            if (totalHp < hp) {
-                totalHp += extra;
-            }
+            totalHp = Math.min(hp, totalHp + extra);
         },
         dealDamage: function (damage) {
-            if (totalHp < damage) {
-                totalHp = 0;
-            } else {
-                totalHp -= damage;
-            }
+            totalHp = Math.max(0, totalHp - damage);
         },
         addWin: function () {
             wins++;
@@ -66,12 +60,10 @@ function battle(fighterFirst, fighterSecond) {
         let competitor = i % modul === 0 ? fighterFirst : fighterSecond;
         let deffender = i % modul === 0 ? fighterSecond : fighterFirst;
         competitor.attack(deffender);
-        i++;
-    }
-    for (const key in arguments) {
-        if (arguments[key].getHealth() === 0) {
-            arguments[key].addLoss();
-            arguments[key].addWin();
+        if (deffender.getHealth() === 0) {
+            deffender.addLoss();
+            competitor.addWin();
         }
+        i++;
     }
 }
